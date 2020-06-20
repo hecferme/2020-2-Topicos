c48982db4e2964a68de14949fd941146bda30a79
trompuditasoftwarein <trompuditasoftwarein@TOPICOS-INSTANC>
2020-06-20 22:48:52 +0000

    Se agrega proyecto en EF Core para conexion con MSLocalDB

---
+++ HelloWorld/EFCoreNorthWind.Model/MisClases/Suppliers.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EFCoreNorthWind.Model.Models
+{
+    public partial class Suppliers
+    {
+        public string CompanyNameUppercase { get {
+                return CompanyName;
+            } set { } }
+    }
+}
---
+++ HelloWorld/EFCoreNorthWind.Model/Models/NorthWindContext.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace EFCoreNorthWind.Model.Models
+{
+    public partial class NorthWindContext : DbContext
+    {
+        public NorthWindContext()
+        {
+        }
+
+        public NorthWindContext(DbContextOptions<NorthWindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Categories> Categories { get; set; }
+        public virtual DbSet<Products> Products { get; set; }
+        public virtual DbSet<Suppliers> Suppliers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=NorthWind;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categories>(entity =>
+            {
+                entity.HasKey(e => e.CategoryId);
+
+                entity.HasIndex(e => e.CategoryName)
+                    .HasName("CategoryName");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.CategoryName)
+                    .IsRequired()
+                    .HasMaxLength(15);
+
+                entity.Property(e => e.Description).HasColumnType("ntext");
+
+                entity.Property(e => e.Picture).HasColumnType("image");
+            });
+
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.HasKey(e => e.ProductId);
+
+                entity.HasIndex(e => e.CategoryId)
+                    .HasName("CategoryID");
+
+                entity.HasIndex(e => e.ProductName)
+                    .HasName("ProductName");
+
+                entity.HasIndex(e => e.SupplierId)
+                    .HasName("SuppliersProducts");
+
+                entity.Property(e => e.ProductId).HasColumnName("ProductID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
+
+                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+                entity.Property(e => e.UnitPrice)
+                    .HasColumnType("money")
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
+
+                entity.HasOne(d => d.Category)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.CategoryId)
+                    .HasConstraintName("FK_Products_Categories");
+
+                entity.HasOne(d => d.Supplier)
+                    .WithMany(p => p.Products)
+                    .HasForeignKey(d => d.SupplierId)
+                    .HasConstraintName("FK_Products_Suppliers");
+            });
+
+            modelBuilder.Entity<Suppliers>(entity =>
+            {
+                entity.HasKey(e => e.SupplierId);
+
+                entity.HasIndex(e => e.CompanyName)
+                    .HasName("CompanyName");
+
+                entity.HasIndex(e => e.PostalCode)
+                    .HasName("PostalCode");
+
+                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
+
+                entity.Property(e => e.Address).HasMaxLength(60);
+
+                entity.Property(e => e.City).HasMaxLength(15);
+
+                entity.Property(e => e.CompanyName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.ContactName).HasMaxLength(30);
+
+                entity.Property(e => e.ContactTitle).HasMaxLength(30);
+
+                entity.Property(e => e.Country).HasMaxLength(15);
+
+                entity.Property(e => e.Fax).HasMaxLength(24);
+
+                entity.Property(e => e.HomePage).HasColumnType("ntext");
+
+                entity.Property(e => e.Phone).HasMaxLength(24);
+
+                entity.Property(e => e.PostalCode).HasMaxLength(10);
+
+                entity.Property(e => e.Region).HasMaxLength(15);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
