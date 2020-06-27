2f9e0c3a1c3ac6171f6cad15af1350990a76228f
trompuditasoftwarein <trompuditasoftwarein@TOPICOS-INSTANC>
2020-06-27 21:27:03 +0000

    Proyecto de DataAccess para consultas de Products

---
+++ HelloWorld/EFCoreNorthWind.DataAccess/Logica/Repositorio/ProductsRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EFCoreNorthWind.DataAccess
+{
+    internal class ProductsRepo
+    {
+        static Model.Models.NorthWindContext _miContexto = new Model.Models.NorthWindContext();
+        public Model.Models.Products GetProductById (int elProductId)
+        {
+            var elResultado = _miContexto.Products.Find(elProductId);
+            return elResultado;
+        }
+        public IList<Model.Models.Products> GetProductsByName(string elProductName)
+        {    
+            var laConsulta = _miContexto.Products.Where
+                        (p => p.ProductName.Contains(elProductName));
+            laConsulta = _miContexto.Products.OrderBy(p => p.ReorderLevel);
+            laConsulta = _miContexto.Products.OrderByDescending(p => p.ProductName);
+            var elResultado = laConsulta.ToList();
+            return elResultado;
+        }
+    }
+}
---
+++ HelloWorld/EFCoreNorthWind.Model/MisClases/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFCoreNorthWind.Model.Models
+{
+    public partial class Products
+    {
+        public short? UnderstockUnits { 
+            get {
+                short? elResultado = 0;
+                if (! this.Discontinued)
+                {
+                    if (this.UnitsInStock + this.UnitsOnOrder < this.ReorderLevel)
+                        elResultado = (short?)(this.UnitsInStock + 
+                            this.UnitsOnOrder - this.ReorderLevel);
+                }
+                return elResultado;
+            } 
+            set { } }
+    }
+}
